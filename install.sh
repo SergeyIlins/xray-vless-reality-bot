be936a654ff8a0635cf0654a5171aa0d7d54833b
SergeyIlins <86128461+SergeyIlins@users.noreply.github.com>
2026-04-15 15:02:39 +0300

    Update install.sh

--- install.sh
+++ install.sh
@@ -36,8 +36,8 @@ fi
 # --- 3. Генерация ключей и shortId ---
 echo -e "${YELLOW}[3/10] Генерация ключей REALITY...${NC}"
 # Генерация ключей (работает с любым форматом вывода xray x25519)
-PRIVATE_KEY=$(xray x25519 | head -1)
-PUBLIC_KEY=$(xray x25519 | tail -1)
+PRIVATE_KEY=$(xray x25519 | head -1 | awk '{print $NF}')
+PUBLIC_KEY=$(xray x25519 | tail -1 | awk '{print $NF}')
 SHORT_ID=$(openssl rand -hex 8)
 if [ -z "$PRIVATE_KEY" ] || [ -z "$PUBLIC_KEY" ]; then
     echo -e "${RED}Не удалось сгенерировать ключи REALITY.${NC}"
